39f8bc3de61ec02f3b7db7b094ccaa1422b46386
Amit Ambardekar <amitamb@gmail.com>
2011-11-07 00:37:45 +0530

    Changed domain to todobin.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,5 +58,5 @@ TodoTimeline::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'todo-bin.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'www.todobin.com' }
 end
