d9a0cff1cf10ed9f90b563143da385ff0c998d65
Amit Ambardekar <amitamb@gmail.com>
2011-11-08 17:46:35 +0530

    Up/down arrow keys support

--- app/assets/javascripts/todos.index.js
+++ app/assets/javascripts/todos.index.js
@@ -17,6 +17,47 @@ $(function(){
 		}
 		$(this).attr("submitted", "true");
 	});
+	
+	$("form.new_todo #todo_text").live("keyup", function(e){
+		if (e.which == 38){
+			var tthis = $(this).parents("li").first().get(0);
+			$lis = $(this).parents("ul").find("li").filter(function(){ return $(this).find("form").length > 0; });
+			$prev_li = null;
+			$lis.each(function(){
+				if ( this == tthis ){
+					if ( $prev_li == null ){
+						$prev_li = $lis.last();
+					}
+					return false;
+				}
+				else
+				{
+					$prev_li = $(this);
+				}
+			});
+			$prev_li.find("#todo_text").focus();
+		}
+		else if (e.which == 40){
+			var tthis = $(this).parents("li").first().get(0);
+			$lis = $(this).parents("ul").find("li").filter(function(){ return $(this).find("form").length > 0; });
+			var tthis_found = false;
+			$next_li = null;
+			$lis.each(function(){
+				if ( tthis_found ){
+					$next_li = $(this);
+					return false;
+				}
+				else if ( this == tthis )
+				{
+					tthis_found = true;
+				}
+			});
+			if ( !$next_li ){
+				$next_li = $lis.first();
+			}
+			$next_li.find("#todo_text").focus();
+		}
+	});
 
 	$("form.new_todo #todo_text").live("focusout", function(){
 		var $form = $(this).parents("form");
