29772b7d33e2591956b75fc67c60b7fd91a20d5e
Amit Ambardekar <amitamb@gmail.com>
2011-11-08 09:31:26 +0530

    Made display of day box up/down based on day's number in a year

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -2,4 +2,8 @@ module TodosHelper
 	def capitalise_sentense(todo_text)
 		todo_text.split(/\. /).map { |sen| sen.capitalize }.join(". ")
 	end
+	
+	def day_in_year(date)
+		(date.to_date - date.to_date.beginning_of_year).to_i
+	end
 end
